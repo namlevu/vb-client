9a3890cd6d342149efe0109c8f2715111ca59377
Vu Le Nam <namvule@hotmail.com>
2018-11-07 17:25:34 +0900

    update package file, add src/index.js

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import '../resources/scss/style.scss';
+
+const element = <h1>Hello,world </h1>;
+ReactDOM.render(element, document.getElementById('root'));
